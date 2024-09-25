c9c16f7de50f4a8be9565ceb31b61d42d78a420a
AndrewPendleton53 <andrew.pendleton@vikings.berry.edu>
2024-09-25 13:47:26 -0400

    More KeyPressed test cases

--- csc121-creative-project/src/SnakeWordTest.java
+++ csc121-creative-project/src/SnakeWordTest.java
@@ -7,16 +7,18 @@ import processing.event.KeyEvent;
 
 class SnakeWordTest {
 	
-	SnakeWorld w1 = new SnakeWorld(new Posn (160, 190), new Posn(300, 200));
-	SnakeWorld w2 = new SnakeWorld(new Posn (180, 190), new Posn(300, 200));
-	SnakeWorld w3 = new SnakeWorld(new Posn (280, 190), new Posn(300, 200));
-	SnakeWorld w4 = new SnakeWorld(new Posn (300, 190), new Posn(300, 200));
+	SnakeWorld w1 =  new SnakeWorld(new Snake (new Posn (180, 210), new Posn (250, 180)), new Posn (-10, 0), 12);
+	SnakeWorld w2 = new SnakeWorld(new Snake (new Posn (170, 205), new Posn (190, 220)), new Posn (0, 10), 20);
+	//SnakeWorld w3 = new SnakeWorld(new Posn (280, 190), new Posn(300, 200));
+	//SnakeWorld w4 = new SnakeWorld(new Posn (300, 190), new Posn(300, 200));
 
 	
 	
 	@Test
 	void testKeyPressed() {
-		assertEquals( w2, w1.keyPressed(new KeyEvent(null, 0, 0, 0, '\0', PApplet.RIGHT)));
+		assertEquals( new SnakeWorld(new Snake (new Posn (180, 210), new Posn (10, 0)), new Posn (-10, 0), 12), w1.keyPressed(new KeyEvent(null, 0, 0, 0, '\0', PApplet.RIGHT)));
+		assertEquals( new SnakeWorld(new Snake (new Posn (170, 205), new Posn (-10, 0)), new Posn (0, 10), 20), w2.keyPressed(new KeyEvent(null, 0, 0, 0, '\0', PApplet.LEFT)));
+		
 	}
 
 }
