ae5aa381ed36e7b128d5c438b59fec3f91590cec
charlespulaski <charles.pulaski@vikings.berry.edu>
2024-09-25 14:22:45 -0400

    made changedir method

--- csc121-creative-project/src/SnakeWordTest.java
+++ csc121-creative-project/src/SnakeWordTest.java
@@ -12,11 +12,11 @@ class SnakeWordTest {
 	SnakeWorld w3 = new SnakeWorld(new Snake(new Posn (20, 10), new Posn (10, 0)), new Posn(5, 10), 5);
 	SnakeWorld w4 = new SnakeWorld(new Snake(new Posn (40, 20), new Posn (0, -10)), new Posn(10, 15), 10);
 	SnakeWorld w5 = new SnakeWorld(new Snake(new Posn (50, 30), new Posn (-10, 0)), new Posn(20, 10), 8);
-	SnakeWorld w6 = new SnakeWorld(new Snake(new Posn (40, 10), new Posn (10, 0)), new Posn(10, 30), 15);
+	SnakeWorld w6 = new SnakeWorld(new Snake(new Posn (40, 10), new Posn (0, 10)), new Posn(10, 30), 15);
 			//new SnakeWorld(new Posn (280, 190), new Posn(300, 200));
 	 
 	//new SnakeWorld(new Posn (300, 190), new Posn(300, 200));
-
+	
 	
 	
 	@Test
@@ -25,6 +25,13 @@ class SnakeWordTest {
 		assertEquals(  new SnakeWorld(new Snake(new Posn (40, 20), new Posn (-10, 0)), new Posn(10, 15), 10), w4.keyPressed(new KeyEvent(null, 0, 0, 0, '\0', PApplet.LEFT)));
 		assertEquals(  new SnakeWorld(new Snake(new Posn (50, 30), new Posn (0, 10)), new Posn(20, 10), 8), w5.keyPressed(new KeyEvent(null, 0, 0, 0, '\0', PApplet.DOWN)));
 		assertEquals(  new SnakeWorld(new Snake(new Posn (40, 10), new Posn (0, -10)), new Posn(10, 30), 15), w6.keyPressed(new KeyEvent(null, 0, 0, 0, '\0', PApplet.UP)));
+	}
+	@Test
+	void testchangeDir() {
+		assertEquals(new Snake(new Posn (20, 10), new Posn (10, 0)), w3.snake.changeDir(new Posn(10, 0)));
+		assertEquals(new Snake(new Posn (40, 20), new Posn (0, -10)), w4.snake.changeDir(new Posn(0, -10)));
+		assertEquals(new Snake(new Posn (50, 30), new Posn (-10, 0)), w5.snake.changeDir(new Posn(-10, 0)));
+		assertEquals(new Snake(new Posn (40, 10), new Posn (0, 10)), w6.snake.changeDir(new Posn(0, 10)));
 	}
 
 }
